163d0303db613e4414fff40e44bb24c258664110
Allison Marino <amarino_us@yahoo.com>
2014-12-15 14:19:37 -0800

    adding basic root files and routes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require("express"),
 	bodyParser = require("body-parser"),
 	app = express(),
 	methodOverride = require("method-override"),
-	pg = require("pg"),
+	pg = require("pg");
 	
 
 app.use(express.static(__dirname + "/public"));
@@ -18,7 +18,30 @@ app.get("/", function (req, res) {
 	res.render("sites/home");
 });
 
+//sign up page
+app.get("/sign_up", function (req, res) {
+	res.render("users/sign_up");
+});
 
+app.post("/sigb_up", function (req, res) {
+
+});
+
+app.get("/login", function (req, res) {
+	res.render("users/login");
+});
+
+app.post("/login", function (req, res) {
+
+});
+
+/*app.get("logout", function (req, res) {
+	res.render("logout");
+})*/
+
+app.get("/userHomepage", function (req, res) {
+	res.render("users/userHomepage");
+});
 
 
 
